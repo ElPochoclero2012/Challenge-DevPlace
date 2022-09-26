7e17fecb0bbafa8f065b22bdb93a5a3e9ac167f2
Marcelo Diego Albano <marce.d.a.2002@gmail.com>
2022-09-26 19:16:46 -0300

    ahora funciona

--- Modulo Algoritmos/Punto 5/Untitled3.cpp
+++ Modulo Algoritmos/Punto 5/Untitled3.cpp
@@ -6,19 +6,21 @@ int nMax=0, nMin=0, nSum=0, num=0;
 
 int main()
 {
-	do //No logro que no tome el -1 como numero minimo ingresado cuando intento finalizar el ingreso de numeros. Si se ingresa un numero menor a ese queda como numero mínimo, pero puede generar problemas
+	do
 	{
 		std::cout<<"Ingrese numeros enteros, cuando quiera frenar ingrese '-1': ";
 		std::cin>>num;		 
-		nSum=+num;	//Variable acumuladora que suma todos los numeros ingresados.
+		if(num!=-1){
+			nSum=nSum+num;	//Variable acumuladora que suma todos los numeros ingresados.
 		
-		if(nMax<num) //Si el numero mas grande ingresado es menor al ultimo que se ingresó se actualiza la variable de este al numero actual.
-		{
-			nMax=num;
-		}	
-		if(nMin>num) //Si el numero mas pequeño ingresado es mayor al ultimo que se ingresó se actualiza la variable de este al numero actual.
-		{
-			nMin=num;
+			if(nMax<num) //Si el numero mas grande ingresado es menor al ultimo que se ingresó se actualiza la variable de este al numero actual.
+			{
+				nMax=num;
+			}	
+			if(nMin>num) //Si el numero mas pequeño ingresado es mayor al ultimo que se ingresó se actualiza la variable de este al numero actual.
+			{
+				nMin=num;
+			}
 		}
 	} while (num!=-1); 
 	
